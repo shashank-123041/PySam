cf2fbe46ec89c5c19206e62a7cfd424b9d3ed088
shashank-123041 <shashankpr23cs@rnsit.ac.in>
2025-02-20 15:33:24 +0530

    optimised bubble sort

--- Day4/bubble_sort.py
+++ Day4/bubble_sort.py
@@ -1,8 +1,18 @@
+
 def bubble_sort(numbers):
-    for i in range(0,len(numbers)):
+    global count
+    count=0
+    for i in range(0,len(numbers)-1):
+        sorted=True
         for j in range(0,len(numbers)-i-1):
+            count+=1
             if(numbers[j]>numbers[j+1]):
                 numbers[j],numbers[j+1]=numbers[j+1],numbers[j]
-n=[2, 3, 4, 1, 5, 8, 6]
+                sorted=False
+        if sorted:
+            #print(count)
+            return
+
+n=[6,5,4,3,2,1]
 bubble_sort(n)
-print(n)
+print(n,count)
