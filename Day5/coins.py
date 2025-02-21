c931fa02b47a75d459ad555d8afbe5c7eae43795
shashank-123041 <shashankpr23cs@rnsit.ac.in>
2025-02-21 16:36:21 +0530

    simmpler code of coins

--- Day5/coins.py
+++ Day5/coins.py
@@ -1,12 +1,4 @@
-coins={10:0,5:0,2:0,1:0}
-amount=int(input("Enter the amount: "))
-for coin in coins.keys():
-    if amount>0:
-        coins[coin]=amount//coin
-        amount-=coins[coin]*coin
-    else:
-        break
-
-print("The number of coins are:")
-for coin in coins:
-    print(f"{coins[coin]} coins of Rs.{coin}")
+coins = {10: 0, 5: 0, 2: 0, 1: 0}
+amount = int(input())
+for coin in coins: coins[coin], amount = amount // coin, amount % coin
+print(*[f"{v} coins of Rs.{k}" for k, v in coins.items() if v], sep="\n")
